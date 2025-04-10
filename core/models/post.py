320b7d9da146fe97d8923bde680712684997aadb
bbtqqrl <makspeys@gmail.com>
2025-04-10 20:44:25 +0200

    Added a relationship user-posts

---
+++ core/models/post.py
@@ -0,0 +1,20 @@
+from sqlalchemy import String, Text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from .base import Base 
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .user import User
+
+class Post(Base):
+    title: Mapped[str] = mapped_column(String(100))
+    body: Mapped[str] = mapped_column(
+        Text,
+        default="",
+        server_default="",
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"),
+    )
+    user: Mapped["User"] = relationship(back_populates="post")
--- core/models/user.py
+++ core/models/user.py
@@ -1,6 +1,13 @@
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .post import Post
 
 class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
+
+    post: Mapped[list["Post"]] = relationship(back_populates="user")
