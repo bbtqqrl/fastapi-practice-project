84af7bf7fcd4dd4dbb73a2575194c7ef8bd69328
bbtqqrl <makspeys@gmail.com>
2025-04-11 20:02:33 +0200

    Create profile for users and mixins

---
+++ core/models/profile.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String, Text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from .base import Base 
+from .mixins import UserRelationMixin
+
+class Profile(UserRelationMixin, Base):
+    _user_id_unique = True
+    _user_back_populates = "profile"
+    first_name: Mapped[str | None] = mapped_column(String(40))
+    last_name: Mapped[str | None] = mapped_column(String(40))
+    bio: Mapped[str | None]
